476eccacfdf408d4ed018244b6d0e051ddf90b62
Hyunseup Jo <soup1997@naver.com>
2024-08-21 19:43:57 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240821/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240821/최소공배수 구하기/find-the-least-common-multiple.py
@@ -4,4 +4,9 @@ for i in range(2, m+1):
     if (n % i == 0) and (m % i == 0):
         maximum = i
 
-print(int(n/maximum * m/maximum * maximum))
+a = n / maximum
+b = m / maximum
+c = maximum
+
+res = int(a*b*c)
+print(res)
