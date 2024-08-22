18cae9ed4e2160a836a1dd01604afe71317b33ed
Hyunseup Jo <soup1997@naver.com>
2024-08-22 16:28:45 +0900

    Title: 행복한 수열의 개수, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240822/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240822/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,11 +4,10 @@ def row_search(row):
         if arr[row][j-1] == arr[row][j]:
             cnt += 1
     
-    if cnt >= m:
-        return 1
+            if cnt >= m:
+                return 1
     
-    else:
-        return 0
+    return 0
 
 def col_search(col):
     cnt = 1
@@ -16,19 +15,13 @@ def col_search(col):
         if arr[i-1][col] == arr[i][col]:
             cnt += 1
 
-    if cnt >= m:
-        return 1
-    
-    else:
-        return 0
+            if cnt >= m:
+                return 1
+    return 0
 
 # make grid
 n, m = map(int, input().split())
-
-arr = []
-for i in range(n):
-    vec = list(map(int, input().split()))
-    arr.append(vec)
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 if m == 1:
     print(2*n)
