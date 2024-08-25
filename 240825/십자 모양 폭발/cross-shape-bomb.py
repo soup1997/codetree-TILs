e1fa165009784b71f8250847673cfc83ba0a2bc6
Hyunseup Jo <soup1997@naver.com>
2024-08-25 15:35:37 +0900

    Title: 십자 모양 폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240825/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+r, c = map(int, sys.stdin.readline().split())
+r, c = r-1, c-1
+
+size = (arr[r][c]) - 1
+
+# explosion
+for i in range(r-size, r+1+size):
+    for j in range(c-size, c+1+size):       
+        if j > n-1:
+            break
+
+        if j == c or i == r:
+            arr[i][j] = 0 
+
+    if i > n-1:
+        break
+
+# arrangement
+for row in range(n-1, 0, -1):
+    for col in range(n):
+        if arr[row][col] == 0:
+            arr[row][col] = arr[row-1][col]
+            arr[row-1][col] = 0
+
+for vec in arr:
+    print(*vec)
