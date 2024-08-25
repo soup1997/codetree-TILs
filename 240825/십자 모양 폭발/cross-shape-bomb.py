521f880715c8a5776c3b093fe30797edc8c07161
Hyunseup Jo <soup1997@naver.com>
2024-08-25 15:41:35 +0900

    Title: 십자 모양 폭발, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240825/십자 모양 폭발/cross-shape-bomb.py
+++ 240825/십자 모양 폭발/cross-shape-bomb.py
@@ -3,21 +3,18 @@ import sys
 n = int(sys.stdin.readline())
 arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 r, c = map(int, sys.stdin.readline().split())
-r, c = r-1, c-1
 
+r, c = r-1, c-1
 size = (arr[r][c]) - 1
 
 # explosion
-for i in range(r-size, r+1+size):
-    for j in range(c-size, c+1+size):       
+for i in range(max(0, r-size), min(n, r+1+size)):
+    for j in range(max(0, c-size), min(n, c+1+size)):       
         if j > n-1:
             break
 
         if j == c or i == r:
             arr[i][j] = 0 
-
-    if i > n-1:
-        break
 
 # arrangement
 for row in range(n-1, 0, -1):
