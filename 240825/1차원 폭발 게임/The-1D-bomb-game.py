84663ba7afa7827e25d43fc3889741649e15efb6
Hyunseup Jo <soup1997@naver.com>
2024-08-25 16:37:32 +0900

    Title: 1차원 폭발 게임, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240825/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240825/1차원 폭발 게임/The-1D-bomb-game.py
@@ -4,29 +4,33 @@ n, m = map(int, sys.stdin.readline().split())
 
 bombs = [int(sys.stdin.readline()) for _ in range(n)]
 
-def explosion(bombs):
-    cnt = 1
+def explosion(bombs, m):
+    if m == 1:
+        return []
+    
+    else:
+        cnt = 1
 
-    for i in range(len(bombs)-1):
-        try:
-            if bombs[i] == bombs[i+1]:
-                cnt += 1
+        for i in range(len(bombs)-1):
+            try:
+                if bombs[i] == bombs[i+1]:
+                    cnt += 1
 
-                if cnt >= m:
-                    del bombs[i:i+cnt]
-                    
-            
-            else:
-                cnt = 1
+                    if cnt >= m:
+                        del bombs[i:i+cnt]
+                        
+                
+                else:
+                    cnt = 1
 
-        except IndexError:
-            explosion(bombs)
-
-
-    return bombs
+            except IndexError:
+                explosion(bombs, m)
 
 
-res = explosion(bombs)
+        return bombs
+
+
+res = explosion(bombs, m)
 
 print(len(res))
 
