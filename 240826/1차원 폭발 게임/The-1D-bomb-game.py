fdf9535f4a1dd2da4200f8290c659db7fc298128
Hyunseup Jo <soup1997@naver.com>
2024-08-26 20:06:38 +0900

    Title: 1차원 폭발 게임, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240826/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240826/1차원 폭발 게임/The-1D-bomb-game.py
@@ -4,19 +4,21 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 vec = [int(sys.stdin.readline()) for _ in range(n)]
 
+n, m = 5 ,2
+vec = [1, 2, 2, 1, 1]
+
 
 cnt = 1
 
 for i in range(n-1):
     if vec[i] == vec[i+1]:
         cnt += 1
-
         if cnt >= m:
             vec[i:i+cnt] = [0 for _ in range(cnt)] # explosion
-            for k in range(n-2): # shift
-                if vec[k] != 0:
-                    vec[k+1] = vec[k]
-                    vec[k] = 0
+            for j in range(i+cnt-1): # shift
+                if vec[j] != 0:
+                    vec[j+1] = vec[j]
+                    vec[j] = 0
             
             cnt = 1 # count initialization
 
