cc49f91a0526f7096ac3be04736b92f6c42de5e5
Hyunseup Jo <soup1997@naver.com>
2024-08-28 16:59:57 +0900

    Title: 그래프 탐색, Time: 191ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/그래프 탐색/graph-traversal.py
+++ 240828/그래프 탐색/graph-traversal.py
@@ -4,22 +4,23 @@ n, m = map(int, sys.stdin.readline().split())
 visited = [False] * n
 visited[0] = True # 1번 노드부터 시작하므로 방문처리
 
-graph = [[0 for _ in range(n)] for _ in range(n)] # 인접 행렬 이용
+graph = [[] for _ in range(n)]
 
-# 그래프 생성
+# 그래프 생성 (Linked list 이용)
 for _ in range(m):
     x, y = map(int, sys.stdin.readline().split())
-    
-    graph[x-1][y-1] = 1 # 연결 되어 있음  
-    graph[y-1][x-1] = 1 # 연결 되어 있음
+    graph[x-1].append(y-1)
+    graph[y-1].append(x-1)
 
 def DFS(vertex):
-    for i in range(n):
-        if (graph[vertex][i] == 1) and (not visited[i]): # 그래프가 연결되어있고 아직 방문하지 않은 정점이라면
-            visited[i] = True # 방문처리
-            DFS(i)
+    for i in range(len(graph[vertex])):
+        currV = graph[vertex][i]
+        if not visited[currV]:
+            visited[currV] = True
+            DFS(currV)
 
-if __name__=='__main__':
+if __name__=="__main__":
     root = 0
     DFS(root)
+
     print(visited.count(True)-1)
