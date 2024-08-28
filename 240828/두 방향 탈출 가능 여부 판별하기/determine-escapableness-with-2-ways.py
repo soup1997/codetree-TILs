6f21b0f9fa5c9d71f2b819307c89cb87c5699aa6
Hyunseup Jo <soup1997@naver.com>
2024-08-28 18:16:57 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240828/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240828/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,25 +1,23 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-visited = [[False for _ in range(m)] for _ in range(n)]
-visited[0][0] = True
+grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)] # 그리드 생성
+visited = [[False for _ in range(m)] for _ in range(n)] # grid의 (행, 열)마다 visited 표시를 위한 2차원 배열
+visited[0][0] = True # 시작점이므로 방문 표시
+dxs, dys = [1, 0], [0, 1] # 아래, 우측으로만 이동
 
-x, y = 0, 0
-dxs, dys = [1, 0], [0, 1]
-
-def inRange(newX, newY):
+def inRange(newX, newY): # grid 범위 내 인지 검사
     return (0 <= newX) and (newX <= n) and (0 <= newY) and (newY <= m)
 
-def snake(newX, newY):
+def snake(newX, newY): # 해당 grid에 뱀이 있는지 검사
     answer = True if grid[newX][newY] else False
     return answer
 
-def visitCheck(newX, newY):
+def visitCheck(newX, newY): # 방문한 grid인지 검사
     answer = True if visited[newX][newY] else False
     return answer
 
-def can_go(newX, newY):
+def can_go(newX, newY): # 이동 여부 체크
     return inRange(newX, newY) and snake(newX, newY) and visitCheck(newX, newY)
 
 def DFS(cx, cy):
@@ -30,4 +28,8 @@ def DFS(cx, cy):
             visited[newX][newY] = 1
             DFS(newX, newY)
 
-print(1) if visited[-1][-1] else print(0)
+
+if __name__=="__main__":
+    x, y = 0, 0 # 초기 시작 위치
+    DFS(x, y)
+    print(1) if visited[-1][-1] else print(0)
