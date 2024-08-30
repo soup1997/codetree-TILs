96c3c634ca0056e3e5c4ba869ca1c4bb1a9ef8e0
Hyunseup Jo <soup1997@naver.com>
2024-08-30 15:39:56 +0900

    Title: 최단 Run Length 인코딩, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240830/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240830/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -11,27 +11,27 @@ def shiftOnce(x):
 
     return ''.join(x) # list를 다시 string으로 변경
 
-def runLengthEncoding(x):
+def runLengthEncoding(x): # 제일 중요한 부분
     result = ""
-    i = 0
+    i = 0 # 초기 인덱스
 
-    while i < len(x):
-        char = x[i]
-        cnt = 1
+    while i < len(x): # 순회
+        char = x[i] # 현재 기준 문자
+        cnt = 1 # 카운트 1부터 시작
 
-        while (i + 1 < len(x) and x[i+1] == char):
+        while (i + 1 < len(x) and x[i+1] == char): # 다음 인덱스가 문자열 길이보다 작고, 다음 인덱스의 문자가 현재 기준 문자와 같을때까지 탐색
             cnt += 1
             i += 1
         
         result += char + str(cnt)
-        i += 1
+        i += 1 # 이렇게 하면 문자가 다른것부터 탐색 시작
 
     return len(result)
 
 if __name__=="__main__":
-    string = sys.stdin.readline().strip()
+    string = sys.stdin.readline().strip() # 문자열 입력받을땐 그냥 무조건 strip 사용하기
 
-    if len(string) == 1:
+    if len(string) == 1: # 예외 처리
         print(2)
 
     else:
