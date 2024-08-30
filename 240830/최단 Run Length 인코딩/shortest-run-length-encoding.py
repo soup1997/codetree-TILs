5f06fef663735022df267ce2d2400032f7e995f3
Hyunseup Jo <soup1997@naver.com>
2024-08-30 15:33:27 +0900

    Title: 최단 Run Length 인코딩, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240830/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240830/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -3,8 +3,10 @@ import sys
 def shiftOnce(x):
     tmp = x[-1]
     x = list(x) # string은 값 변경이 불가능함, 리스트로 변경
+    
     for i in range(len(x)-1, 0, -1):
         x[i] = x[i-1] # shift 적용
+
     x[0] = tmp
 
     return ''.join(x) # list를 다시 string으로 변경
@@ -21,15 +23,15 @@ def runLengthEncoding(x):
             cnt += 1
             i += 1
         
-        result += str(char) + str(cnt)
+        result += char + str(cnt)
         i += 1
-    
+
     return len(result)
 
 if __name__=="__main__":
     string = sys.stdin.readline()
 
-    if len(string) == 1:
+    if len(string) == 2:
         print(2)
 
     else:
