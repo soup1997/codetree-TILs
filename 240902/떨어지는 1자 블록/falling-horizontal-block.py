6f28f6d43a06081fe9e4676493359e14458583bf
Hyunseup Jo <soup1997@naver.com>
2024-09-02 20:03:27 +0900

    Title: 떨어지는 1자 블록, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240902/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240902/떨어지는 1자 블록/falling-horizontal-block.py
@@ -2,28 +2,22 @@ import sys
 
 n, m, k = map(int, sys.stdin.readline().split())
 grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
 block = [1] * m
 k = k-1
 
 def fallingDown(k):
-    max_row, max_column = (0, k + m - 1)
-    empty = True
+    max_row = 0
 
     for i in range(n):
-        for j in range(k, k+m):
-            if grid[i][j] == 1:
-                empty = not empty
-                break
-
-        if empty:
+        result = any(grid[i][k:k+m]) # 해당 열에서 블록 크기 만큼 슬라이싱했을때 하나라도 1이면 True, 못집어 넣음
+        
+        if not result:
             max_row = max(max_row, i)
 
-        else:
-            empty = True
+    return max_row
 
-    return max_row, max_column
-
-max_row, max_column = fallingDown(k)
+max_row, max_column = fallingDown(k), k + m - 1
 grid[max_row][k:max_column+1] = block
 
 for row in grid:
