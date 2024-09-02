c2756e6734f2d0266d63abed84d86d1cfb6192bd
Hyunseup Jo <soup1997@naver.com>
2024-09-02 16:17:29 +0900

    Title: 양수 직사각형의 최대 크기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240902/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240902/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(1000)
 
 n, m = map(int, sys.stdin.readline().split())
 grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
