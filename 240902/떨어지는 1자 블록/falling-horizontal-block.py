5853c84564a6eb0ef066530d194c28d509fb97d1
Hyunseup Jo <soup1997@naver.com>
2024-09-02 20:11:02 +0900

    Title: 떨어지는 1자 블록, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240902/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240902/떨어지는 1자 블록/falling-horizontal-block.py
@@ -8,15 +8,21 @@ k = k-1
 
 def fallingDown(k):
     max_row = 0
+    memory = True
 
     for i in range(n):
-        result = any(grid[i][k:k+m]) # 해당 열에서 블록 크기 만큼 슬라이싱했을때 하나라도 1이면 True, 하강 불가
+        check = any(grid[i][k:k+m]) # 해당 열에서 블록 크기 만큼 슬라이싱했을때 하나라도 1이면 True, 하강 불가
         
-        if not result: # 모두다 0 이라면 블록 하강 가능
+        if not check and not memory: # 모두다 0 이라면 블록 하강 가능
             max_row = max(max_row, i)
             break
+        
+        else:
+            memory = not memory
 
     return max_row
+
+        
 
 max_row, max_column = fallingDown(k), k + m - 1
 grid[max_row][k:max_column+1] = block
