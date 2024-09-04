872cbdfbb85890fab78f122b581d7e72960dffad
Hyunseup Jo <soup1997@naver.com>
2024-09-04 22:14:12 +0900

    Title: 갈 수 있는 곳들, Time: 186ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240904/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,48 @@
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().split())
+grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+vertices = [list(map(int, sys.stdin.readline().split())) for _ in range(k)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+q = deque()
+cnt = 1
+
+def inRange(x, y):
+    return (0 <= x) and (x < n) and (0 <= y) and (y < n)
+
+def isVisited(x, y):
+    return visited[x][y]
+
+def isZero(x, y):
+    return grid[x][y] == 0
+
+def canGo(x, y):
+    return inRange(x, y) and not isVisited(x, y) and isZero(x, y)
+
+def bfs(start_x, start_y):
+    global cnt, directions, q
+
+    q.append((start_x, start_y))
+    visited[start_x][start_y] = True
+
+    while q:
+        x, y = q.popleft()
+
+        for direction in directions:
+            dx, dy = direction[0], direction[1]
+            X, Y = x + dx, y + dy
+
+            if canGo(X, Y):
+                visited[X][Y] = True
+                cnt += 1
+                q.append((X, Y))
+
+
+if __name__=="__main__":
+    for vertex in vertices:
+        x, y = vertex[0] - 1 , vertex[1] - 1
+        bfs(x, y)
+    
+    print(cnt)
