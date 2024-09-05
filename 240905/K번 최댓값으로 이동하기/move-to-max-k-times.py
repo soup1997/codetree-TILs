d027551aa0c2ba0caac477a77ef11d7319bede33
Hyunseup Jo <soup1997@naver.com>
2024-09-05 22:06:53 +0900

    Title: K번 최댓값으로 이동하기, Time: 207ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240905/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 240905/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -8,37 +8,36 @@ from collections import deque
 n, k = map(int, sys.stdin.readline().split())
 grid = [(list(map(int, sys.stdin.readline().split()))) for _ in range(n)]
 r, c = map(int, sys.stdin.readline().split())
-r, c = r-1, c-1
-directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+r, c = r-1, c-1 # 1씩 빼서 받기
+directions = [[-1, 0], [1, 0], [0, -1], [0, 1]] # 인접 4방향
 
-sx, sy = r, c
+sx, sy = r, c # 시작 기준 좌표
 
-def inRange(x, y):
+def inRange(x, y): # grid 바깥을 넘어가지 않는지 검사
     return (0 <= x) and (x < n) and (0 <= y) and (y < n)
 
-def isVisited(x, y):
+def isVisited(x, y): # 방문한 좌표인지 검사
     return visited[x][y]
 
-def isSmaller(x, y, ref):
+def isSmaller(x, y, ref): # 기준 좌표보다 작은 값을 가지고 있는지 검사
     return grid[x][y] < ref
 
-def canGo(x, y, ref):
+def canGo(x, y, ref): # 3가지 조건을 모두 충족해야 함
     return inRange(x, y) and not isVisited(x, y) and isSmaller(x, y, ref)
 
+for _ in range(k):
+    maxval = 0 # grid 내에 1이상 100이하의 숫자만 존재하므로 0으로 초기화
+    visited = [[False for _ in range(n)] for _ in range(n)] # visited 초기화
 
-
-for _ in range(k): # k번 반복
-    maxval = 0  
-    visited = [[False for _ in range(n)] for _ in range(n)]
-
+    # bfs 시작
     q = deque()
-    q.append((sx, sy))
-    visited[sx][sy] = True
+    q.append((sx, sy)) # 기준 좌표 넣고 시작
+    visited[sx][sy] = True # 기준 좌표 방문 처리
 
-    while q:
+    while q: # queue가 빌때까지 계속 반복
         x, y = q.popleft()
 
-        for direction in directions:
+        for direction in directions: # 인접 4방향 검사
             dx, dy = direction[0], direction[1]
             X, Y = x + dx, y + dy
 
@@ -46,16 +45,18 @@ for _ in range(k): # k번 반복
                 q.append((X, Y))
                 visited[X][Y] = True
 
-                if grid[X][Y] > maxval:
-                    maxval = grid[X][Y]
-                    sx, sy = X, Y
+                # maxval 값이 하나라면 
+                if grid[X][Y] > maxval: # 현재 탐색하고 있는 좌표값이 maxval보다 크다면
+                    maxval = grid[X][Y] # maxval 업데이트, 애초에 canGo에서 기준좌표 값 보다 작은 값만 움직이게 했으므로 기준 좌표 값보다 무조건 작은 값임
+                    sx, sy = X, Y # 해당 최댓값을 다음 for문 시작 좌표로 설정
 
+                # maxval 값이 두개 이상이라면
                 elif grid[X][Y] == maxval:
-                    if sx > X:
+                    if sx > X: # 현재 탐색 x좌표가 시작 x좌표보다는 무조건 작아야 함 (우선순위 1)
                         sx, sy = X, Y
                     
-                    elif sx == X:
-                        if sy > Y:
+                    else: # 현재 탐색 x좌표와 시작 x좌표값과 같다면 (우선순위 2)
+                        if sy > Y: # 현재 탐색 
                             sx, sy = X, Y
     
     r, c = sx, sy
