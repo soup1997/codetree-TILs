0594a4976724680d1675a197ab61052d56d42f94
Hyunseup Jo <soup1997@naver.com>
2024-09-06 20:31:48 +0900

    Title: 떨어지는 1자 블록, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240906/떨어지는 1자 블록/falling-horizontal-block.py
@@ -6,14 +6,13 @@ grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 block = [1] * m
 block_range = [k-1, k+m-1]
 
-
 max_row = 0
 
 for i in range(len(grid)):
     if grid[i][k-1:k+m-1].count(0) == m:
         max_row = max(max_row, i)
     
-    else:
+    else: # max row를 찾으면서 중간에 블록 막혀있으면 더 이상 탐색할 필요 없음
         break
 
 grid[max_row][k-1:k+m-1] = block
