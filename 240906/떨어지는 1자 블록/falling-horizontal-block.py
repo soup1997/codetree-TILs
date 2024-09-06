9d3232ddfae523dd9012f7a4ffec74428f6514da
Hyunseup Jo <soup1997@naver.com>
2024-09-06 20:13:46 +0900

    Title: 떨어지는 1자 블록, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240906/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,15 @@
+import sys
+
+n, m, k = map(int, sys.stdin.readline().split())
+grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+block = [1] * m
+block_range = [k-1, k+m-1]
+
+
+for i in range(1, len(grid)):
+    if any(grid[i][k-1:k+m-1]):
+        grid[i-1][k-1:k+m-1] = block
+        break
+
+for row in grid:
+    print(*row)
