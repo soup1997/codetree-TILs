ef605c4e0889cdfb8d4f2eb66020ac0d1c7f84f6
Hyunseup Jo <soup1997@naver.com>
2024-09-06 20:14:11 +0900

    Title: 떨어지는 1자 블록, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240906/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240906/떨어지는 1자 블록/falling-horizontal-block.py
@@ -6,7 +6,7 @@ block = [1] * m
 block_range = [k-1, k+m-1]
 
 
-for i in range(1, len(grid)):
+for i in range(len(grid)):
     if any(grid[i][k-1:k+m-1]):
         grid[i-1][k-1:k+m-1] = block
         break
