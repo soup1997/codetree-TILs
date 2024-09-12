d5d81abfc2eef556a294c214a7b1ee6f51913498
Hyunseup Jo <soup1997@naver.com>
2024-09-12 20:59:19 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 153ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240912/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 240912/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -3,10 +3,11 @@ import sys
 n = int(sys.stdin.readline().strip())
 nums = list(map(int, sys.stdin.readline().split()))
 total = 0
+
 for i in nums:
     total += i
 
     if total < 0:
-        total = 0
+        total = i
 
 print(total)
