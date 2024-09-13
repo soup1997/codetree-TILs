ff082c0fb4f5a14e8999538aa5aa94da90148cf0
Hyunseup Jo <soup1997@naver.com>
2024-09-13 20:35:45 +0900

    Title: 회의실 준비 구현, Time: 307ms, Memory: 31MB, Status: Passed - CodeTree

--- 240913/회의실 준비 구현/implement-scheduling-meeting-room.py
+++ 240913/회의실 준비 구현/implement-scheduling-meeting-room.py
@@ -4,21 +4,12 @@ n = int(sys.stdin.readline())
 m = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 m.sort(key=lambda x: x[1])
 
-idx = 0
-cnt = 0
-while idx < len(m)-1:
-    if m[idx][1] <= m[idx+1][0]:
+cnt = 1
+now = m[0][1]
+
+for i in range(1, n): # 감탄스러운 코드다, i값을 전혀 건드릴 필요가 없다
+    if now <= m[i][0]:
         cnt += 1
-        idx += 1
-
-    else:
-        before_end = m[idx][1]
-
-        while idx < len(m)-1:
-            if before_end <= m[idx+1][0]:
-                cnt += 1
-                break
-
-            idx += 1
-
-print(cnt+1)
+        now = m[i][1] # 조건 맞을때 만 기준 값을 바꾼다
+    
+print(cnt)
