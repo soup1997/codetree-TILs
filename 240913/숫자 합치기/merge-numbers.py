1ab6bb352ed1ba7764d45b4bdccbb2909f4307c4
Hyunseup Jo <soup1997@naver.com>
2024-09-13 19:25:03 +0900

    Title: 숫자 합치기, Time: 383ms, Memory: 31MB, Status: Passed - CodeTree

--- 240913/숫자 합치기/merge-numbers.py
+++ 240913/숫자 합치기/merge-numbers.py
@@ -1,18 +1,16 @@
 import sys
+import heapq # 힙을 이용해서 빠르게 최소값을 찾아내자!
 
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
+heapq.heapify(arr)
 cost = 0
 
 while len(arr) >= 2:
-    first_idx = arr.index(min(arr))
-    first_min = arr.pop(first_idx)
-
-    second_idx = arr.index(min(arr))
-    second_min = arr.pop(second_idx)
-
+    first_min = heapq.heappop(arr)
+    second_min = heapq.heappop(arr)
     res = first_min + second_min
+    heapq.heappush(arr, res)
     cost += (res)
-    arr.append(res)
 
 print(cost)
