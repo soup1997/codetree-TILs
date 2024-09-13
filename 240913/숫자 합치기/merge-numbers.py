fb39713886a413bec818749649d85d4dc34aaffb
Hyunseup Jo <soup1997@naver.com>
2024-09-13 19:19:26 +0900

    Title: 숫자 합치기, Time: 5039ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240913/숫자 합치기/merge-numbers.py
+++ 240913/숫자 합치기/merge-numbers.py
@@ -5,13 +5,14 @@ arr = list(map(int, sys.stdin.readline().split()))
 cost = 0
 
 while len(arr) >= 2:
-    arr.sort() # 정렬 먼저 시작
+    first_idx = arr.index(min(arr))
+    first_min = arr.pop(first_idx)
 
-    res = arr[0] + arr[1]
+    second_idx = arr.index(min(arr))
+    second_min = arr.pop(second_idx)
+
+    res = first_min + second_min
     cost += (res)
-
-    arr.pop(0)
-    arr.pop(0)
     arr.append(res)
 
 print(cost)
