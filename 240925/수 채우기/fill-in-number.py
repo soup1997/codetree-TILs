b5ce5360198a5dfaec640f4389548beaec8eb1fb
Hyunseup Jo <soup1997@naver.com>
2024-09-25 20:07:52 +0900

    Title: 수 채우기, Time: 108ms, Memory: 25MB, Status: Passed - CodeTree

--- 240925/수 채우기/fill-in-number.py
+++ 240925/수 채우기/fill-in-number.py
@@ -10,8 +10,14 @@ ans = MAX_NUM
 # 그 중 가장 좋은 선택을 합니다.
 for i in range(MAX_NUM + 1):
     remainder = n - 5 * i
-    if remainder >= 0 and remainder % 2 == 0:
+    
+    if remainder < 0:
+        break
+    
+    elif remainder >= 0 and remainder % 2 == 0:
         ans = min(ans, i + (remainder // 2))
+    
+    
 
 # 만약 그러한 동전을 만드는 것이 불가능하다면
 # -1을 넣어줍니다.
