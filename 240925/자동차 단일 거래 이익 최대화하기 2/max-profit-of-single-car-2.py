a47b8a01fabea79bb30c09b9230908e534606553
Hyunseup Jo <soup1997@naver.com>
2024-09-25 20:27:55 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 5015ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 240925/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+price = list(map(int, sys.stdin.readline().split()))
+
+profit = 0
+
+# 가장 쌀 때 사서 가장 비싸게 파는 것이 목적
+# 단, 년도는 역행이 불가능함
+
+for i in range(len(price)):
+    for j in range(i+1, len(price)):
+        if price[j] - price[i] > 0:
+            profit = max(profit, price[j] - price[i])
+
+print(profit)
