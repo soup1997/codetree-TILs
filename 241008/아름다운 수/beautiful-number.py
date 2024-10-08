6a223e2e58b3539217f7162bd150e75a32d9dca5
Hyunseup Jo <soup1997@naver.com>
2024-10-08 15:01:35 +0900

    Title: 아름다운 수, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/아름다운 수/beautiful-number.py
@@ -0,0 +1,35 @@
+import sys
+
+answer = 0
+n = int(sys.stdin.readline())
+
+def check(num_list):
+    global answer
+
+    for i in range(len(num_list)):
+        standard = num_list[i]
+        cnt = 0
+        while(i < len(num_list) and standard == num_list[i]):
+            cnt += 1
+            i += 1
+        
+        if cnt == standard:
+            answer += 1
+            
+
+def makeBeautifulNum():
+    global num
+
+    if len(num) == n:
+        check(num)
+        return
+
+    else:
+        for i in range(1, 5):
+            num.append(i)
+            makeBeautifulNum()
+            num.pop()
+
+num = []
+makeBeautifulNum()
+print(answer)
