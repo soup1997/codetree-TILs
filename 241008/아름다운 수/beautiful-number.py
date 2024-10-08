7ac7b288ba1bda82e8d867a67c574128531bab96
Hyunseup Jo <soup1997@naver.com>
2024-10-08 15:19:00 +0900

    Title: 아름다운 수, Time: 527ms, Memory: 29MB, Status: Passed - CodeTree

--- 241008/아름다운 수/beautiful-number.py
+++ 241008/아름다운 수/beautiful-number.py
@@ -12,14 +12,11 @@ def check(num_list):
     while i < len(num_list):
         standard = num_list[i]
         cnt = 0
-
         while i < len(num_list) and standard == num_list[i]:
             cnt += 1
-            i += 1
-        
-        if cnt != standard:
+            i += 1        
+        if (cnt != standard) and (cnt % standard != 0): # 반례 추가, 111인경우 1이 1번 나온것이 3번반복되었으므로 아름다운 수
             flag = False
-
     if flag:
         answer += 1
             
